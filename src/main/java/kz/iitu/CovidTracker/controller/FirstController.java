611ed2b7d6c267494e9035a7d9a88d2dfbb93b2b
OlzhasXD <o.olzhaev@gmail.com>
2020-05-18 00:16:18 +0600

    CovidTracker v0.1

--- src/main/java/kz/iitu/CovidTracker/controller/FirstController.java
+++ src/main/java/kz/iitu/CovidTracker/controller/FirstController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -27,6 +29,24 @@ public class FirstController {
             difference += country.getDifference();
         }
         model.addAttribute("countries", dataService.getAllCountry());
+        model.addAttribute("total", total);
+        model.addAttribute("difference", difference);
+        return "homePage";
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam String filter, Model model) {
+        List<Country> temp = dataService.getAllCountry();
+        List<Country> countryFilter = new ArrayList<>();
+        int total = 0, difference = 0;
+        for(Country country : temp) {
+            if(country.getCountry().equals(filter) || country.getState().equals(filter)) {
+                countryFilter.add(country);
+                total += country.getNumberOfCases();
+                difference += country.getDifference();
+            }
+        }
+        model.addAttribute("countries", countryFilter);
         model.addAttribute("total", total);
         model.addAttribute("difference", difference);
         return "homePage";
